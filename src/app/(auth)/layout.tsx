5b5172e9cff816be8c6483b6e39d7968897892d2
John Mason <87030324+simkidd@users.noreply.github.com>
2024-08-17 03:40:27 +0100

    authlayout and login form

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/providers/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.scss";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +25,20 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div>{children}</div>
+          <div className="w-full min-h-dvh lg:grid lg:grid-cols-2">
+            <div className="flex items-center justify-center py-12">
+              {children}
+            </div>
+            <div className="hidden bg-muted lg:block">
+              <Image
+                src="/placeholder.svg"
+                alt="Image"
+                width="1920"
+                height="1080"
+                className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              />
+            </div>
+          </div>
         </ThemeProvider>
       </body>
     </html>
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import LoginForm from "../components/LoginForm";
+import Link from "next/link";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="mx-auto grid gap-6">
+      <div className="grid gap-2 text-center">
+        <h1 className="text-3xl font-bold">Login</h1>
+        <p className="text-balance text-muted-foreground">
+          Enter your email below to login to your account
+        </p>
+      </div>
+      <LoginForm />
+      <div className="mt-4 text-center text-sm">
+        Don&apos;t have an account?{" "}
+        <Link href="#" className="underline">
+          Sign up
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
